354ad2c9bd8fb6fd981623c34e26876e155d8ccd
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-02-01 01:24:16 +0900

    Create Supervisor for Game

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -1,13 +1,14 @@
 defmodule IslandsEngine.Game do
-  use GenServer
+  use GenServer, start: {__MODULE__, :start_link, []}, restart: :transient
   alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
 
   @players [:player1, :player2]
+  @timeout 60 * 60 * 24 * 1000
 
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
     player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
-    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}}
+    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}, @timeout}
   end
 
   def start_link(name) when is_binary(name),
@@ -16,6 +17,10 @@ defmodule IslandsEngine.Game do
   def handle_info(:first, state) do
     IO.puts("This message has been handled by handle_info/2, matching on :first.")
     {:noreply, state}
+  end
+
+  def handle_info(:timeout, state_data) do
+    {:stop, {:shutdown, :timeout}, state_data}
   end
 
   def handle_call({:add_player, name}, _from, state_data) do
@@ -122,7 +127,9 @@ defmodule IslandsEngine.Game do
 
   defp update_rules(state_data, rules), do: %{state_data | rules: rules}
 
-  defp reply_success(state_data, reply), do: {:reply, reply, state_data}
+  defp reply_success(state_data, reply) do
+    {:reply, reply, state_data, @timeout}
+  end
 
   defp player_board(state_data, player), do: Map.get(state_data, player).board
 
