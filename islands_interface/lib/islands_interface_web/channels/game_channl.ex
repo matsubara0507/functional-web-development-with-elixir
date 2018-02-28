256076418eda84a32547457aa4c35bc34ed2621d
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-02-28 17:24:39 +0900

    Feat: new game, add second player, position island

--- islands_interface/lib/islands_interface_web/channels/game_channl.ex
+++ islands_interface/lib/islands_interface_web/channels/game_channl.ex
@@ -1,8 +1,48 @@
 defmodule IslandsInterfaceWeb.GameChannel do
   use IslandsInterfaceWeb, :channel
-  alias IslandsEngine.{Game, GameSupervisor}
+  alias IslandsEngine.{Game, GameSupervisor, Island}
 
   def join("game:" <> _player, _payload, socket) do
     {:ok, socket}
+  end
+
+  def handle_in("new_game", _payload, socket) do
+    "game:" <> player = socket.topic
+
+    case GameSupervisor.start_game(player) do
+      {:ok, _pid} ->
+        {:reply, :ok, socket}
+
+      {:error, reason} ->
+        {:reply, {:error, %{reason: inspect(reason)}}, socket}
+    end
+  end
+
+  def handle_in("add_player", player, socket) do
+    case Game.add_player(via(socket.topic), player) do
+      :ok ->
+        broadcast!(socket, "player_added", %{message: "New player just joined: " <> player})
+        {:noreply, socket}
+
+      {:error, reason} ->
+        {:reply, {:error, %{reason: inspect(reason)}}, socket}
+
+      :error ->
+        {:reply, :error, socket}
+    end
+  end
+
+  defp via("game:" <> player), do: Game.via_tuple(player)
+
+  def handle_in("position_island", payload, socket) do
+    Island.types()
+    %{"player" => player, "island" => island, "row" => row, "col" => col} = payload
+    player = String.to_existing_atom(player)
+    island = String.to_existing_atom(island)
+
+    case Game.position_island(via(socket.topic), player, island, row, col) do
+      :ok -> {:reply, :ok, socket}
+      _ -> {:reply, :error, socket}
+    end
   end
 end
